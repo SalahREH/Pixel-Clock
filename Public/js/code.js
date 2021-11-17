56bc0db93510d47342fd02094fe37f299d49de7c
Bubbl3Waffl3 <salahlfd0@gmail.com>
2021-11-17 20:13:59 +0100

    v.0.1.2 with music

--- Public/js/code.js
+++ Public/js/code.js
@@ -56,3 +56,15 @@ function randombgmobile() {
 
 document.querySelector(".change-bg").addEventListener("click", randombg)
 document.querySelector(".change-bg").addEventListener("click", randombgmobile)
+let audio1 = new Audio("./music/713.mp3")
+function playbgm(){
+    audio1.play();
+    console.log("hola");
+}
+function pausebgm(){
+    audio1.pause();
+    console.log("patimicola");
+}
+
+document.querySelector(".play-button").addEventListener("click",playbgm)
+document.querySelector(".pause-button").addEventListener("click",pausebgm)
