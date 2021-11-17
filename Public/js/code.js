c6affd09c0d6afe681f06cac3bf5a524870fcf04
Bubbl3Waffl3 <salahlfd0@gmail.com>
2021-11-17 18:38:46 +0100

    The Pixel Clock v0.1.1 Mobile version included

--- Public/js/code.js
+++ Public/js/code.js
@@ -34,11 +34,21 @@ let bg4 = `url("../img/gif-background4.gif")`
 let bg5 = `url("../img/gif-background5.gif")`
 let bg6 = `url("../img/gif-background6.gif")`
 let arraybg = [bg1 , bg2 , bg3 , bg4 , bg5 ,]
+let bgm1 = `url("../img/gif-background.gif")`
+let bgm2 = ``
+let bgm3 = ``
+let arraybgmobile = [bgm1, bgm2 , bgm3]
 
 function randombg() {
     let rdm = arraybg[Math.floor(Math.random() * arraybg.length)]
     document.documentElement.style.setProperty("--bg" , rdm)
     console.log(rdm);
 }
+function randombgmobile() {
+    let rdm = arraybgmobile[Math.floor(Math.random() * arraybgmobile.length)]
+    document.documentElement.style.setProperty("--bgmobile" , rdm)
+    console.log(rdm);
+}
 
 document.querySelector(".change-bg").addEventListener("click", randombg)
+document.querySelector(".change-bg").addEventListener("click", randombgmobile)
